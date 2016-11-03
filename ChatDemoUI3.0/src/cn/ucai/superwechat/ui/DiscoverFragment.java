d1bd7cebfa3f2df9a3df842183d272a6c0195ab8
Matao <1229450981@qq.com>
2016-11-04 00:51:06 +0800

    编写MainActivity的布局文件

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/DiscoverFragment.java
@@ -0,0 +1,21 @@
+package cn.ucai.superwechat.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.ucai.superwechat.R;
+
+/**
+ * Created by 涛 on 2016/11/4.
+ */
+
+public class DiscoverFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dicover, container, false);
+    }
+
+}
--- easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
+++ easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
@@ -1,6 +1,7 @@
 package com.hyphenate.easeui.utils;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,7 +14,7 @@ import com.hyphenate.easeui.domain.EaseUser;
 import com.hyphenate.easeui.domain.User;
 
 public class EaseUserUtils {
-
+    private static final String TAG = EaseUserUtils.class.getSimpleName();
     static EaseUserProfileProvider userProvider;
 
     static {
@@ -100,6 +101,7 @@ public class EaseUserUtils {
     public static void setAppUserNick(String username, TextView textView) {
         if (textView != null) {
             User user = getAppUserInfo(username);
+            Log.e(TAG,"user="+user);
             if (user != null && user.getMUserNick() != null) {
                 textView.setText(user.getMUserNick());
             } else {
