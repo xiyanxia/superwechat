1d651250c6f662cab4662bcad88bb6d7b0c40315
Matao <1229450981@qq.com>
2016-11-03 23:41:08 +0800

    修改登录过程连续返回导致空指针问题

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/LoginActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/LoginActivity.java
@@ -284,6 +284,8 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        pd.dismiss();
+        if(pd!=null) {
+            pd.dismiss();
+        }
     }
 }
