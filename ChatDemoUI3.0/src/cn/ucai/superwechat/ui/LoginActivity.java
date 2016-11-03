806304b2f3b6f2a8da02bcd16216c086aaa0a514
Matao <1229450981@qq.com>
2016-11-04 00:20:38 +0800

    完善登录页面跳转注册,回到登录页面显示最新注册的账号

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/LoginActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/LoginActivity.java
@@ -264,6 +264,9 @@ public class LoginActivity extends BaseActivity {
         if (autoLogin) {
             return;
         }
+        if (SuperWeChatHelper.getInstance().getCurrentUsernName() != null) {
+            mEtUsername.setText(SuperWeChatHelper.getInstance().getCurrentUsernName());
+        }
     }
 
     @OnClick({R.id.img_back, R.id.btn_login, R.id.btn_register})
