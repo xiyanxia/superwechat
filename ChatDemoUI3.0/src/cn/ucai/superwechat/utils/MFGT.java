8b5aafbcbcfbf049864537d7a17a0c61d0233e3f
Matao <1229450981@qq.com>
2016-11-06 22:58:17 +0800

    添加二维码图标、修改SettingsFragment为SettingsActivity

--- ChatDemoUI3.0/src/cn/ucai/superwechat/utils/MFGT.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/utils/MFGT.java
@@ -14,30 +14,38 @@ import cn.ucai.superwechat.ui.RegisterActivity;
  * 辅助跳转
  */
 public class MFGT {
-    public static void finish(Activity activity){
+    public static void finish(Activity activity) {
         activity.finish();
-        activity.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
     }
-    public static void startActivity(Activity context,Class<?> cls){
+
+    public static void startActivity(Activity context, Class<?> cls) {
         Intent intent = new Intent();
-        intent.setClass(context,cls);
-        startActivity(context,intent);
+        intent.setClass(context, cls);
+        startActivity(context, intent);
     }
-    public static void startActivity(Context context,Intent intent){
+
+    public static void startActivity(Context context, Intent intent) {
         context.startActivity(intent);
-        ((Activity)context).overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+        ((Activity) context).overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
     }
-    public static void startActivityForResult(Activity context,Intent intent,int requestCode){
-        context.startActivityForResult(intent,requestCode);
-        context.overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+
+    public static void startActivityForResult(Activity context, Intent intent, int requestCode) {
+        context.startActivityForResult(intent, requestCode);
+        context.overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
     }
-    public static void gotoLogin(Activity context){
+
+    public static void gotoLogin(Activity context) {
         startActivity(context, LoginActivity.class);
     }
-    public static void gotoRegister(Activity context){
+
+    public static void gotoRegister(Activity context) {
         startActivity(context, RegisterActivity.class);
     }
 
+    public static void gotoSettings(Activity context) {
+        startActivity(context, SettingsActivity.class);
+    }
 
 
 }
